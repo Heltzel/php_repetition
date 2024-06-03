e3f958db9bbcc2db4f2c9b0d8fbb60093d171e92
Heltzel <heltzel.service@hotmail.com>
2024-06-03 15:44:30 +0200

    PHP router

---
+++ views/404.php
@@ -0,0 +1,15 @@
+<?php require('views/partials/head.php'); ?>
+<?php require('views/partials/nav.php'); ?>
+
+<main>
+	<div class="mt-20">
+		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 text-center ">
+			<h1 class="text-4xl font-bold">Sorry, page not found</h1>
+			<h2 class="text-2xl font-bold mt-4">404</h2>
+			<p class="mt-4">
+				<a href="/" class="text-blue-600 underline text-xl">Back to Homepage</a>
+			</p>
+		</div>
+	</div>
+</main>
+<?php require('views/partials/footer.php'); ?>
